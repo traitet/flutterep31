a21ab1ffac76334e3f9f1e1a8e3a9cdd9abb45ed
Traitet <traitet@gmail.com>
2020-08-13 01:05:56 +0700

    minor change main

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'See-Flutter เรียนรู้ไปด้วยกัน START EP21'),
+      home: MyHomePage(title: 'See-Flutter เรียนรู้ไปด้วยกัน START EP31'),
     );
   }
 }
